5c5da4831055f2402d784837b44a8a77221fbe6e
Candy-Tewari <sansshobhit@gmail.com>
2020-11-14 06:45:28 +0530

    Changing expiresAt to createdAt.

--- models/refresh.js
+++ models/refresh.js
@@ -13,7 +13,7 @@ let schema = new mongoose.Schema({
         type: String,
         required: true
     },
-    expiresAt: {
+    createdAt: {
         type: Date,
         default: Date.now,
         expires: '30d' 
